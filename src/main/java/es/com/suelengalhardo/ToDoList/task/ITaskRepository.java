17fdeded0b3f4f76bfbeaf7be24d99d1df29c942
SuelenGalhardo <suelenranucci@gmail.com>
2024-08-15 14:01:12 +0200

    added Ittask and TaskController

---
+++ src/main/java/es/com/suelengalhardo/ToDoList/task/ITaskRepository.java
@@ -0,0 +1,10 @@
+package es.com.suelengalhardo.ToDoList.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository  extends JpaRepository<TaskModel, UUID>{
+
+    
+} 
---
+++ src/main/java/es/com/suelengalhardo/ToDoList/task/TaskController.java
@@ -0,0 +1,31 @@
+package es.com.suelengalhardo.ToDoList.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+
+    @Autowired 
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public String postMethodName(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        return entity;
+    }
+    
+    public TaskModel create (@RequestBody TaskModel taskModel){
+         var task = this.taskRepository.save(taskModel);
+         return task;
+
+    }
+    
+}
