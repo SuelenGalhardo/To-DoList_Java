d963eadbf3d68b80d89fa5347f5ad373b4c9185e
SuelenGalhardo <suelenranucci@gmail.com>
2024-08-19 17:44:39 +0200

    added update tasks update pacials list tasks

--- src/main/java/es/com/suelengalhardo/ToDoList/task/TaskController.java
+++ src/main/java/es/com/suelengalhardo/ToDoList/task/TaskController.java
@@ -2,6 +2,7 @@ package es.com.suelengalhardo.ToDoList.task;
 
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
-//import es.com.suelengalhardo.ToDoList.utils.Utils;
+import es.com.suelengalhardo.ToDoList.utils.Utils;
 
 @RestController
 @RequestMapping("/tasks")
@@ -50,6 +53,39 @@ public class TaskController {
         return  ResponseEntity.status(HttpStatus.OK).body(task);
 	
 	}
+
+     @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+        // var idUser = request.getAttribute("idUser");
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Usuário não tem permissão  para alterar essa tarefa");
+        }
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        // taskModel.setIdUser((UUID) idUser);
+        // taskModel.setId(id);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
+    }
 }
+
 
 	
